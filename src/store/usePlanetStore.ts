893fcf0359220c22cfbdaad4fa15f8138699f2ee
gs18050 <dodream40910@gmail.com>
2025-01-06 21:46:48 +0900

    same sizes

--- src/store/usePlanetStore.ts
+++ src/store/usePlanetStore.ts
@@ -61,7 +61,7 @@ export const usePlanetStore = create<PlanetStore>((set) => ({
       z: Math.random() * 20 - 10   // -10 ~ 10 사이
     };
   
-    const modelPath = `/models/planet${Math.floor(Math.random() * 16) + 1}.glb`;
+    var modelPath = `/models/planet${Math.floor(Math.random() * 16) + 1}.glb`;
     
     // 백엔드에 먼저 저장
     const savedBucket = await createBucketList({
@@ -69,10 +69,69 @@ export const usePlanetStore = create<PlanetStore>((set) => ({
       modelPath,
       position,
     });
-  
+
     // 스케일 계산 (더 작은 값으로 조정)
-    const scale = 1.5; // 기본 스케일을 4에서 1.5로 축소
-  
+    var maxDimension = 1.0
+
+    switch (modelPath) {
+      case "/models/planet1.glb":
+        maxDimension = 5.162075066733811;
+        break;
+      case "/models/planet2.glb":
+        maxDimension = 2.003995937491723;
+        break;
+      case "/models/planet3.glb":
+        maxDimension = 2.068285665802876;
+        break;
+      case "/models/planet4.glb":
+        maxDimension = 18.64741790433749;
+        break;
+      case "/models/planet5.glb":
+        maxDimension = 555.1829357002869;
+        break;
+      case "/models/planet6.glb":
+        maxDimension = 2.459632131912585;
+        break;
+      case "/models/planet7.glb":
+        maxDimension = 47.976548450897035;
+        break;
+      case "/models/planet8.glb":
+        maxDimension = 27.830789706460514;
+        break;
+      case "/models/planet9.glb":
+        maxDimension = 3.1097603327519856;
+        break;
+      case "/models/planet10.glb":
+        maxDimension = 229.96562576293945;
+        break;
+      case "/models/planet11.glb":
+        maxDimension = 328.8452072428718;
+        break;
+      case "/models/planet12.glb":
+        maxDimension = 12.39915707081638;
+        break;
+      case "/models/planet13.glb":
+        maxDimension = 2.0494765820914247;
+        break;
+      case "/models/planet14.glb":
+        maxDimension = 2.003998052547473;
+        break;
+      case "/models/planet15.glb":
+        maxDimension = 396.18216839936946;
+        break;
+      case "/models/planet16.glb":
+        maxDimension = 57.41700152805447;
+        break;
+      case "/models/planet17.glb":
+        maxDimension = 151.12920389586276;
+        break;
+      default:
+        maxDimension = 1; // 기본값 설정 (필요한 경우)
+        break;
+    }
+
+    const scale = 3 / maxDimension
+
     const newPlanet: Planet = {
       id: savedBucket._id,
       content,
@@ -136,7 +195,8 @@ export const usePlanetStore = create<PlanetStore>((set) => ({
         ...state.planetPositionsAndScales,
         [id]: { position, scale },
       },
-    })),
+    }
+  )),
 
   // ✅ 모든 버킷리스트 가져오기
   
